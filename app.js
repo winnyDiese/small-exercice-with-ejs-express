029a33de7921bb3904f81f204763609c328cfe6e
winnyDiese <winnydiese@gmail.com>
2024-05-22 13:35:00 +0200

    lkblybkuyb

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express()
 app.set('views','./views')
 app.set('view engine','ejs')
 
+// Var
 let message = "Suis un super message..."
 
+// Route
 app.get('/',(req,res)=>{
     res.render('home',{message})
+})
+app.get('/contact',(req,res)=>{
+    res.render('pages/contact')
 })
 
 app.listen(3001)
